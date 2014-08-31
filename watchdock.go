0da9c2abed953e7d399ca93dc73bf22497c7252f
Matt <brimstone@the.narro.ws>
2014-08-31 14:18:09 -0400

    Adding spew support to catch a troublesome error.

--- watchdock.go
+++ watchdock.go
@@ -6,7 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/armon/consul-api"
-	//"github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"
 	dockerclient "github.com/fsouza/go-dockerclient"
 	"log"
 	"strconv"
@@ -90,6 +90,7 @@ func findContainerByName(name string, running bool) (*dockerclient.Container, er
 	if err != nil {
 		log.Fatal(err)
 	}
+	spew.Dump(runningContainers)
 	for _, c := range runningContainers {
 		// If we find one
 		if c.Names[0] == "/"+name {
